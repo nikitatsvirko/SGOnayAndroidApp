8fdb54e386ff9d088aded72968931b538fab1898
Nikita <nikitatsvirko2013@outlook.com>
2017-04-15 15:42:13 +0300

    Design updated

--- app/src/main/java/com/application/nikita/sgonayapp/activities/LoginActivity.java
+++ app/src/main/java/com/application/nikita/sgonayapp/activities/LoginActivity.java
@@ -3,7 +3,6 @@ package com.application.nikita.sgonayapp.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -28,7 +27,6 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().setTitle(R.string.title_activity_login);
-
     }
 
     public void onClicked(View view) {
